f6a1f5e12188a0c8ec616bc6aac03053eceb4fd4
Tuan Dang <dangminhtuan.arwen@gmail.com>
2021-04-05 14:24:12 +0700

    [Bài tập] Hiển thị các số nguyên tố nhỏ hơn 100

--- thePrime.java
+++ thePrime.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class thePrime {
+
+public class thePrime {
+    public static void main(String[] args) {
+
+        int number=2;
+
+        while(number<100){
+            int i = 2;
+            boolean check = true;
+            while (i <= Math.sqrt(number)) {
+                if (number % i == 0) {
+                    check = false;
+                    break;
+                }
+                i++;
+            }
+            if (check) {
+                System.out.print(number + " ");
+            } number++;
+        }
+    }
 }
