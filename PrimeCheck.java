17be34c01eed56db1c582e5a65c02bdc06124513
Tuan Dang <dangminhtuan.arwen@gmail.com>
2021-04-05 14:04:18 +0700

    fix

--- PrimeCheck.java
+++ PrimeCheck.java
@@ -1,27 +1,26 @@
-import java.util.Scanner;
-
 public class PrimeCheck {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Nhập số: ");
-        int number = sc.nextInt();
 
-        if (number<2){
-            System.out.println("Không phải số nguyên tố");
-        } else {
-            int i = 2;
-            boolean check = true;
-            while (i<Math.sqrt(number)){
-                if (number % i == 0){
-                    check = false;
-                    break;
-                } i++;
-            }
-            if (check){
-                System.out.println(number + " Là số nguyên tố");
-            } else {
-                System.out.println(number + " Không phải số nguyên tố");
+                int i = 2;
+                boolean check = true;
+                int number;
+
+                java.util.Scanner input = new java.util.Scanner(System.in);
+                System.out.println("Enter a number: ");
+                number = input.nextInt();
+
+                while (i <= Math.sqrt(number)) {
+                    if (number % i == 0) {
+                        check = false;
+                        break;
+                    }
+                    i++;
+                }
+
+                if (check)
+                    System.out.println(number + " is a prime.");
+                else
+                    System.out.println(number + " is not a prime.");
+
             }
         }
-    }
-}
